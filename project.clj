525216af56f9a5738cac4da4972eef00c3a9be3c
Martin Klepsch <martinklepsch@googlemail.com>
2018-10-17 15:41:59 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -4,15 +4,16 @@
   :warn-on-reflection false
   :license {:name "MIT"
             :url "http://www.opensource.org/licenses/mit-license.php"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [slingshot "0.12.1"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [slingshot "0.12.2"]]
   :profiles {:dev {:dependencies [[ring/ring-jetty-adapter "1.3.2"]
                                   [ring/ring-devel "1.3.2"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha5"]]}}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}}
   :test-selectors {:default  #(not (:integration %))
                    :integration :integration
                    :all (constantly true)}
-  :aliases {"all" ["with-profile" "dev,1.4:dev,1.5:dev:dev,1.7"]}
+  :aliases {"all" ["with-profile" "dev,1.4:dev,1.5:dev,1.6:dev,1.7:dev,1.8"]}
   :checksum-deps true)
