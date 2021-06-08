777f5c90f9929a8990a6fa6d606cc655fd7868c6
vemv <vemv@users.noreply.github.com>
2021-06-08 23:55:45 +0200

    `:test-selectors`: default to running all tests
    This is safer, especially for the CI integration.

--- project.clj
+++ project.clj
@@ -11,8 +11,9 @@
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}}
-  :test-selectors {:default  #(not (:integration %))
-                   :integration :integration
-                   :all (constantly true)}
+  :test-selectors {:default     (constantly true)
+                   :all         (constantly true)
+                   :unit        #(not (:integration %))
+                   :integration :integration}
   :aliases {"all" ["with-profile" "dev,1.4:dev,1.5:dev,1.6:dev,1.7:dev,1.8:dev,1.9"]}
   :checksum-deps true)
