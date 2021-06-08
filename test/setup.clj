f0b7aeb4b4d03e501d8f5d0c538d53077a365b65
vemv <vemv@users.noreply.github.com>
2021-06-08 23:57:21 +0200

    Setup logging
    This silences Jetty logging, which can be otherwise noisy, especially when using the `:port 0` option.

---
+++ test/setup.clj
@@ -0,0 +1,22 @@
+(ns setup
+  "This namespace will be automaticaly loaded by the test runner"
+  (:require
+   [clojure.string :as string])
+  (:import
+   (org.eclipse.jetty.util MultiException)))
+
+(-> (reify Thread$UncaughtExceptionHandler
+      (uncaughtException [_ thread e]
+        ;; Omit exceptions coming from "Address already in use" because they're meaningless
+        ;; (these happen when one picks port 0, and after one such exception a new port will be retried successfully)
+        (let [omit? (and (instance? MultiException e)
+                         (->> ^MultiException e
+                              .getThrowables
+                              (every? (fn [^Throwable t]
+                                        (-> t .getMessage (string/includes? "Address already in use"))))))]
+          (when-not omit?
+            (-> ^Throwable e .printStackTrace)
+            (when (System/getenv "CI")
+              (System/exit 1))))))
+
+    (Thread/setDefaultUncaughtExceptionHandler))
