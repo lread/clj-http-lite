38f7bfd30c3e299a052c6a1497371b7b06ef5323
Michiel Borkent <michielborkent@gmail.com>
2021-11-18 07:37:54 +0100

    Add maybe-deploy

--- build.clj
+++ build.clj
@@ -1,5 +1,6 @@
 (ns build
-  (:require [clojure.tools.build.api :as b]))
+  (:require [clojure.string :as str]
+            [clojure.tools.build.api :as b]))
 
 (def lib 'org.clj-commons/clj-http-lite)
 (def version (format "0.4.%s" (b/git-count-revs nil)))
@@ -22,6 +23,11 @@
   (b/jar {:class-dir class-dir
           :jar-file jar-file}))
 
+(def release-marker "Release-")
+
+(defn extract-version [tag]
+  (str/replace-first tag release-marker ""))
+
 (defn deploy [opts]
   (jar opts)
   ((requiring-resolve 'deps-deploy.deps-deploy/deploy)
@@ -30,3 +36,20 @@
            :pom-file (b/pom-path {:lib lib :class-dir class-dir})}
           opts))
   opts)
+
+(defn maybe-deploy [opts]
+  (if-let [tag (System/getenv "CIRCLE_TAG")]
+    (do
+      (println "Found tag " tag)
+      (if (re-find (re-pattern release-marker) tag)
+        (do
+          (println "Releasing to clojars...")
+          (-> opts
+              (assoc :lib lib :version (extract-version tag))
+              (deploy)))
+        (do
+          (println "Tag is not a release tag, skipping deploy")
+          opts)))
+    (do
+      (println "No tag found, skipping deploy")
+      opts)))
