bb10221cd823cacf2ecaef6e5588a6bb3e1efe39
Michiel Borkent <michielborkent@gmail.com>
2021-11-18 07:30:33 +0100

    Add initial build.clj

---
+++ build.clj
@@ -0,0 +1,32 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def lib 'org.clj-commons/clj-http-lite)
+(def version (format "0.4.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis basis
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
+
+(defn deploy [opts]
+  (jar opts)
+  ((requiring-resolve 'deps-deploy.deps-deploy/deploy)
+   (merge {:installer :remote
+           :artifact jar-file
+           :pom-file (b/pom-path {:lib lib :class-dir class-dir})}
+          opts))
+  opts)
