23e59e5a56f6cb1bf8df15a2365bd696468c17a2
Michiel Borkent <michielborkent@gmail.com>
2021-11-18 16:06:21 +0100

    Bump version in README

--- bb/tasks.clj
+++ bb/tasks.clj
@@ -1,9 +1,16 @@
 (ns tasks
-  (:require [clojure.string :as str]))
+  (:require [babashka.tasks :refer [shell]]
+            [clojure.string :as str]))
 
 (defn replace-version [file version cc]
+  (prn version cc)
   (spit file
         (str/replace (slurp file)
                      (re-pattern (format "(%s)\\.(\\d+)" version))
                      (fn [[_ version _]]
                        (str version "." cc)))))
+
+(defn git-count-revs []
+  (-> (shell {:out :string} "git rev-list HEAD --count")
+      :out
+      str/trim))
