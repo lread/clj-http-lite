386f82336bad7f4c6e1e0986fae9c9490e2b9662
Michiel Borkent <michielborkent@gmail.com>
2021-11-18 15:26:41 +0100

    Bump version in README

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.martinklepsch/clj-http-lite "0.4.278"
+(defproject org.martinklepsch/clj-http-lite "0.4.280"
   :description "A Clojure HTTP library similar to clj-http, but more lightweight."
   :url "https://github.com/martinklepsch/clj-http-lite/"
   :license {:name "MIT"
