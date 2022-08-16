3bc07af867d8ff5affc3f5e1a0300d1921d1438f
lread <lee@dlread.com>
2022-08-16 12:17:43 -0400

    tests: rename test namespaces to end in -test
    It is what most folks and tools expect these days.
    
    Closes #35

--- test/clj_http/test/core_test.clj
+++ test/clj_http/test/core_test.clj
@@ -1,4 +1,4 @@
-(ns clj-http.test.core
+(ns clj-http.test.core-test
   (:require [clj-http.lite.core :as core]
             [clj-http.lite.util :as util]
             [clojure.test :refer [deftest is use-fixtures]]
