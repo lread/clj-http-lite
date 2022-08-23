71564ac79535b7e9140ee82c501f944e876e6b44
lread <lee@dlread.com>
2022-08-23 13:09:43 -0400

    Run entire test suite under Babashka
    We were formerly running a small sanity test for babashka.
    By separating out our test http server to a process,
    we can now hit our full suite with babashka.
    
    We've kept our bb sanity tests but moved them under our full suite.
    (see client-sanity-test ns).
    
    Bb task `test:bb` now runs the full suite.
    
    For consistency, I've re-added `test:jvm` which behaves the same as
    `test:bb` but also supports a `--clj-version` option.
    CI has been updated to use/exercise the test:jvm task.
    
    Also added a simple test reporter for better feedback when testing
    and to make it abundantly clear which platform we are testing on.
    
    See update README for usage.
    
    Some tests initially failed under bb:
    - Tests were checking specifically for thrown
    `java.net.ssl.SSLException` and `java.net.SocketTimeoutException`.
    Neither of these classes are currently available in bb.
    I switched to checking the message only.
    - Encoding tests were testing against "windows-1252" charset.
    Bb does include this charset, but on Windows only.
    I switched the test to use "iso-8859-1" which is available on bb on
    all platforms.
    
    Closes #48

---
+++ test/clj_http/lite/test_util/server_process.clj
@@ -0,0 +1,66 @@
+(ns clj-http.lite.test-util.server-process
+  (:require [clj-http.lite.test-util.server-state :refer [server-state-file]]
+            [clojure.edn :as edn])
+  (:import (java.net URL HttpURLConnection)
+           (java.lang ProcessBuilder$Redirect)))
+
+(defn- url-reachable? [s]
+  (try
+    (let [^HttpURLConnection c (.openConnection (URL. s))]
+      (.setRequestMethod c "GET")
+      (= 200 (.getResponseCode c)))
+    (catch Throwable _e)))
+
+(defn kill [{:keys [^Process process http-port]}]
+  (when (and process (.isAlive process))
+    (println "Stopping http-server")
+    (try
+      (let [^HttpURLConnection c (.openConnection (URL. (format "http://localhost:%d/stop" http-port)))]
+        (.setRequestMethod c "GET")
+        (.getResponseCode c))
+      (catch Throwable e
+        (println "warn: stop command failed\n" (.printStackTrace e))))
+    (.waitFor process)))
+
+(defn launch []
+  (when (.exists server-state-file)
+    (.delete server-state-file))
+  (let [max-wait-msecs 10000 ;; maybe CI is real slow?
+        status-every-ms 1000
+        time-limit (+ (System/currentTimeMillis) max-wait-msecs)
+        ;; use bb's clojure launcher for an easy time on Windows
+        p (-> (ProcessBuilder. ["bb" "clojure" "-X:http-server"])
+              (.redirectOutput ProcessBuilder$Redirect/INHERIT)
+              (.redirectError ProcessBuilder$Redirect/INHERIT)
+              (.start))]
+    (-> (Runtime/getRuntime)
+        (.addShutdownHook (Thread. (fn [] (when (.isAlive p)
+                                            (println "killing http-server forcibly")
+                                            (.destroyForcibly p)
+                                            (.waitFor p))))))
+    (print "Starting http-server")
+    (flush)
+    (loop [next-status (System/currentTimeMillis)
+           server-state nil]
+      (cond
+        (not (.isAlive p))
+        (throw (ex-info "Http-server process died unexpectedly" {}))
+        (> (System/currentTimeMillis) time-limit)
+        (do (when server-state
+              (kill server-state))
+            (throw (ex-info "Timed out waiting for test http-server to start" {})))
+        (and server-state (url-reachable? (format "http://localhost:%d/get"
+                                                  (:http-port server-state))))
+        (do (println)
+            server-state)
+        (and (not server-state) (.exists server-state-file))
+        (recur next-status
+               (-> server-state-file slurp edn/read-string (assoc :process p)))
+        :else
+        (let [next-status (if (> (System/currentTimeMillis) next-status)
+                            (do (print ".")
+                                (flush)
+                                (+ (System/currentTimeMillis) status-every-ms))
+                            next-status)]
+          (Thread/sleep 50)
+          (recur next-status server-state))))))
