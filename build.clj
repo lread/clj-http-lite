5cdeb5f4a029ec21c758b4392931bc292e173cfd
Lee Read <lee@dlread.com>
2022-09-29 17:21:19 -0400

    Automate publishing of release (#69)
    Invoked via `bb publish`
    Read doc/03-maintainer-guide.adoc for details
    
    Details of interest:
    - using neil convention of storing version in deps.edn
    - tagging with v1.2.3 instead of Release-1.2.3
    - now at version 1.x (was 0.x)
    - version patch is now release count, was commit count, next version
    should be 1.0.13
      - hiredman - 4 releases
      - martinklepsch - 4 releases
      - clj-commons - 4 releases
    - addition of anemic pom includes artifact description which gets
    displayed by services like clojars and cljdoc
    
    Closes #65

--- build.clj
+++ build.clj
@@ -1,67 +1,43 @@
 (ns build
-  (:require [build-shared :as shared]
-            [clojure.string :as str]
+  (:require [build-shared]
             [clojure.tools.build.api :as b]))
 
-(def lib 'org.clj-commons/clj-http-lite)
-(def version (shared/version (shared/git-count-revs)))
+(def version (build-shared/lib-version))
+(def lib (build-shared/lib-artifact-name))
+
+;; build constants
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
-(def github-repo "https://github.com/clj-commons/clj-http-lite")
 
-(defn clean [_]
-  (b/delete {:path "target"}))
-
-(defn jar [opts]
-  (let [opts (merge {:class-dir class-dir
+(defn jar [_]
+  (println "jarring version" version)
+  (b/write-pom {:class-dir class-dir
                      :lib lib
                      :version version
+                     :scm {:tag (build-shared/version->tag version)}
                      :basis basis
-                     :src-dirs ["src"]}
-                    opts)
-        opts (assoc opts :scm
-                    (cond-> {:url "https://github.com/clj-commons/clj-http-lite"}
-                      (:tag opts) (assoc :tag (:tag opts))))]
-    (b/write-pom opts))
-  (b/copy-dir {:src-dirs ["src" "resources"]
+                     :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src"]
                :target-dir class-dir})
   (b/jar {:class-dir class-dir
           :jar-file jar-file}))
 
+(defn install [_]
+  (jar {})
+  (println "installing version" version)
+  (b/install {:basis basis
+              :lib lib
+              :version version ;; can't remember why we need to repeat version here, it is in jar-file
+              :jar-file jar-file
+              :class-dir class-dir}))
+
 (defn deploy [opts]
   (jar opts)
+  (println "deploy")
   ((requiring-resolve 'deps-deploy.deps-deploy/deploy)
    (merge {:installer :remote
            :artifact jar-file
            :pom-file (b/pom-path {:lib lib :class-dir class-dir})}
           opts))
   opts)
-
-(def release-marker "Release-")
-
-(defn extract-version [tag]
-  (str/replace-first tag release-marker ""))
-
-(defn ci-tag []
-  (when (= "tag" (System/getenv "GITHUB_REF_TYPE"))
-    (System/getenv "GITHUB_REF_NAME")))
-
-(defn maybe-deploy [opts]
-  (if-let [tag (ci-tag)]
-    (do
-      (println "Found tag " tag)
-      (if (re-find (re-pattern release-marker) tag)
-        (do
-          (println "Releasing to clojars...")
-          (-> opts
-              (assoc :lib lib
-                     :version (extract-version tag)
-                     :tag tag)
-              (deploy)))
-        (do
-          (println "Tag is not a release tag, skipping deploy")
-          opts)))
-    (do
-      (println "No tag found, skipping deploy")
-      opts)))
